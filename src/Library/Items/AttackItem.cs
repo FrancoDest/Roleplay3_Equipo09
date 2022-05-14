5e6b3d7fd0328395481f22c521b372a8f70226f7
[Santiago Severo] <[santiagosevero28@gmail.com]>
2022-05-14 17:44:41 -0300

    Agregue Attack y DefenseItem

---
+++ src/Library/Items/AttackItem.cs
@@ -0,0 +1,7 @@
+namespace RoleplayGame
+{
+    public abstract class AttackItem : IAttackItem
+    {
+        public virtual int AttackValue { get; }
+    }
+}
---
+++ src/Library/Items/DefenseItem.cs
@@ -0,0 +1,7 @@
+namespace RoleplayGame
+{
+    public abstract class DefenseItem : IDefenseItem
+    {
+        public virtual int DefenseValue {get;}
+    } 
+}
