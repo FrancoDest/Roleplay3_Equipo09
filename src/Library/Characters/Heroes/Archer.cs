e9465728cd2e72bfc16766514e702725025af1d8
Seba-Quintana <SEBASTIAN.QUINTANA@correo.ucu.edu.uy>
2022-05-19 02:01:15 -0300

    Comentarios en heroes y personajes

--- src/Library/Characters/Heroes/Archer.cs
+++ src/Library/Characters/Heroes/Archer.cs
@@ -3,10 +3,10 @@ namespace RoleplayGame
 {
     public class Archer: Hero 
     {
+        // Se crea un arquero, el cual hereda de heroe y comienza con arco y casco.
         public Archer(string name)
         {
             this.Name = name;
-            
             this.AddItem(new Bow());
             this.AddItem(new Helmet());
         }
--- src/Library/Characters/Heroes/Wizard.cs
+++ src/Library/Characters/Heroes/Wizard.cs
@@ -3,10 +3,10 @@ namespace RoleplayGame
 {
     public class Wizard: MagicHero
     {
+        // Se crea un mago, el cual hereda de heroe magico y comienza con un baculo.
         public Wizard(string name)
         {
             this.Name = name;
-            
             this.AddItem(new Staff());
         }
     }
