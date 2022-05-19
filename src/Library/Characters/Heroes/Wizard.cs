966be169eb4e129e15bb1afda16986560c7ac4d1
Seba-Quintana <SEBASTIAN.QUINTANA@correo.ucu.edu.uy>
2022-05-18 22:42:05 -0300

    arreglo en herencia

--- src/Library/Characters/Heroes/Wizard.cs
+++ src/Library/Characters/Heroes/Wizard.cs
@@ -1,72 +1,13 @@
 using System.Collections.Generic;
 namespace RoleplayGame
 {
-    public class Wizard: Hero 
+    public class Wizard: MagicHero
     {
-        private List<IMagicalItem> magicalItems = new List<IMagicalItem>();
-
         public Wizard(string name)
         {
             this.Name = name;
             
             this.AddItem(new Staff());
         }
-        
-        public override int AttackValue
-        {
-            get
-            {
-                int value = 0;
-                foreach (IItem item in this.items)
-                {
-                    if (item is IAttackItem)
-                    {
-                        value += (item as IAttackItem).AttackValue;
-                    }
-                }
-                foreach (IMagicalItem item in this.magicalItems)
-                {
-                    if (item is IMagicalAttackItem)
-                    {
-                        value += (item as IMagicalAttackItem).AttackValue;
-                    }
-                }
-                return value;
-            }
-        }
-
-        public override int DefenseValue
-        {
-            get
-            {
-                int value = 0;
-                foreach (IItem item in this.items)
-                {
-                    if (item is IDefenseItem)
-                    {
-                        value += (item as IDefenseItem).DefenseValue;
-                    }
-                }
-                foreach (IMagicalItem item in this.magicalItems)
-                {
-                    if (item is IMagicalDefenseItem)
-                    {
-                        value += (item as IMagicalDefenseItem).DefenseValue;
-                    }
-                }
-                return value;
-            }
-        }
-
-        public void AddItem(IMagicalItem item)
-        {
-            this.magicalItems.Add(item);
-        }
-
-        public void RemoveItem(IMagicalItem item)
-        {
-            this.magicalItems.Remove(item);
-        }
-
     }
 }
