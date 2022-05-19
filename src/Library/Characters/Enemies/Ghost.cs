1877b89ff9bb31dd2ed96326a0717606513a3c50
[Santiago Severo] <[santiagosevero28@gmail.com]>
2022-05-19 01:53:34 -0300

    Agrego Comentarios en Enemies

--- src/Library/Characters/Enemies/Ghost.cs
+++ src/Library/Characters/Enemies/Ghost.cs
@@ -1,18 +1,18 @@
 namespace RoleplayGame
 {
-    public class Ghost : MagicEnemy
+    public class Ghost : MagicEnemy //Se crea una clase llamada Ghost, heredada de la clase MagicEnemy
     {
         public override int VP
         {
             get 
             {
-                return 3;
+                return 3; //Este número se trata de VP (Puntos de victoria) que va para el heroe que lo derrote.
             }
         }
         public Ghost (string name)
         {
             this.Name = name;
-            this.AddItem(new Staff());
+            this.AddItem(new Staff());// Al enemigo se le equipa un arma: un báculo, que sirve tanto para ataque como defensa.
         }
         
     }
--- src/Library/Characters/Enemies/Skeleton.cs
+++ src/Library/Characters/Enemies/Skeleton.cs
@@ -1,19 +1,19 @@
 namespace RoleplayGame
 {
-    public class Skeleton : Enemy
+    public class Skeleton : Enemy //Creamos la clase Skeleton, que es heredada de la clase abstracta Enemy.
     {
         public override int VP
         {
             get 
             {
-                return 2;
+                return 2; //Otorga la cantidad de puntos asignada para al que lo derrote.
             }
         }
         public Skeleton (string name)
         {
             this.Name = name;
-            this.AddItem(new Sword());
-            this.AddItem(new Helmet());
+            this.AddItem(new Sword());//Le agregamos una espada. 
+            this.AddItem(new Helmet());//Le equipamos un casco aumentando su defensa.
         }
         
     }
