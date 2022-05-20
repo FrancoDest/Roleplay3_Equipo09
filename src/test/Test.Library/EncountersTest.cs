9e2e5cdddfc4c095267367847455e81435763e31
Franco De Stefano <FRANCO.DESTEFANO@correo.ucu.edu.uy>
2022-05-19 21:05:08 -0300

    Cambios a encounter y por lo tanto a encounter test

--- src/test/Test.Library/EncountersTest.cs
+++ src/test/Test.Library/EncountersTest.cs
@@ -1,5 +1,6 @@
 using RoleplayGame;
 using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace Test.Library
 {
@@ -15,48 +16,32 @@ namespace Test.Library
         private Orc orcTest2;
         private Skeleton skeletonTest;
         private Encounters encounter;
+        private List<Hero> herolist;
+        private List<Enemy> enemylist;
+
 
         [SetUp]
         public void Setup()
         {
             archerTest = new Archer("Raul");
+
             knightTest = new Knight("Tusam");
+
             dwarfTest = new Dwarf("Thomas");
+
             wizardTest = new Wizard("Ruben");
+
             darkLordTest = new DarkLord("Samuel");
+
             ghostTest = new Ghost("Gonzalo");
+
             orcTest = new Orc("francisc");
+
             orcTest2 = new Orc("francisc2");
+
             skeletonTest = new Skeleton("robert");
-            encounter = new Encounters();
-        }
 
-        /*
-        Prueba que no se realicen ataques si solo se añade un heroe a las
-        listas de personaje de encuentros (sin ningun enemigo)
-        */
-        [Test]
-        public void OnlyHero()
-        {
-            int expected = archerTest.Health;
-            encounter.AddHero(archerTest);
-            encounter.DoEncounter();
-            Assert.AreEqual(expected, archerTest.Health);
         }
-
-        /*
-        Prueba que no se realicen ataques si solo se añade un enemigo a las
-        listas de personaje de encuentros (sin ningun heroe)
-        */
-        [Test]
-        public void OnlyEnemy()
-        {
-            int expected = orcTest.Health;
-            encounter.AddEnemy(orcTest);
-            encounter.DoEncounter();
-            Assert.AreEqual(expected, orcTest.Health);
-        }
-
         /*
         Prueba que se realicen de manera correcta los ataques en un encuentro
         cuando la cantidad de heroes y de enemigos son la misma
@@ -64,8 +49,9 @@ namespace Test.Library
         [Test]
         public void Hero1Enemy1()
         {
-            encounter.AddHero(dwarfTest);
-            encounter.AddEnemy(orcTest);
+            herolist.Add(dwarfTest);
+            enemylist.Add(orcTest);
+            encounter = new Encounters(herolist,enemylist);
             encounter.DoEncounter();
             Assert.AreEqual(72, dwarfTest.Health);
             Assert.AreEqual(0, orcTest.Health);
@@ -78,9 +64,9 @@ namespace Test.Library
         [Test]
         public void Hero2Enemy1()
         {
-            encounter.AddHero(archerTest);
-            encounter.AddHero(knightTest);
-            encounter.AddEnemy(orcTest);
+            herolist.Add(archerTest);
+            herolist.Add(knightTest);
+            enemylist.Add(orcTest);
             encounter.DoEncounter();
             Assert.AreEqual(79, archerTest.Health);
             Assert.AreEqual(100, knightTest.Health);
@@ -94,9 +80,9 @@ namespace Test.Library
         [Test]
         public void Hero1Enemy2()
         {
-            encounter.AddHero(archerTest);
-            encounter.AddEnemy(orcTest);
-            encounter.AddEnemy(skeletonTest);
+            herolist.Add(archerTest);
+            enemylist.Add(orcTest);
+            enemylist.Add(skeletonTest);
             encounter.DoEncounter();
             Assert.AreEqual(0, archerTest.Health);
             Assert.AreEqual(0, orcTest.Health);
@@ -106,10 +92,10 @@ namespace Test.Library
         [Test]
         public void Hero2Enemy2()
         {
-            encounter.AddHero(archerTest);
-            encounter.AddHero(knightTest);
-            encounter.AddEnemy(orcTest);
-            encounter.AddEnemy(skeletonTest);
+            herolist.Add(archerTest);
+            herolist.Add(knightTest);
+            enemylist.Add(orcTest);
+            enemylist.Add(skeletonTest);
             encounter.DoEncounter();
             Assert.AreEqual(0, archerTest.Health);
             Assert.AreEqual(100,knightTest.Health);
@@ -122,57 +108,12 @@ namespace Test.Library
         [Test]
         public void VPAsignados()
         {
-            encounter.AddHero(dwarfTest);
-            encounter.AddEnemy(orcTest);
-            encounter.AddEnemy(orcTest2);
+            herolist.Add(dwarfTest);
+            enemylist.Add(orcTest);
+            enemylist.Add(orcTest2);
             encounter.DoEncounter();
             Assert.AreEqual(100,dwarfTest.Health);
         }
-
-        //El objetivo del test es verificar si se eliminan de la lista los character luego de un encuentro
-        //En el encuentro Hero1Enemy1 dwarf termina con menos vida y el orc termina sin vida
-        //Si el hero que sigue con vida (por lo tanto sigue en la lista) se lo elimina de la misma al terminar el encuentro
-        //al agregar un enemy pero no un hero y llamar a DoEncounter nuevamente
-        //no habria hero en DoEncounter por lo cual la vida de los character no cambiaria ya que no hay batalla
-        [Test]
-        public void CleanListsTest()
-        {
-            encounter.AddHero(dwarfTest);
-            encounter.AddEnemy(orcTest);
-            encounter.DoEncounter();
-            encounter.AddEnemy(skeletonTest);
-            encounter.DoEncounter();
-            Assert.AreEqual(72, dwarfTest.Health);
-            Assert.AreEqual(100, skeletonTest.Health);
-        }
-
-        //El objetivo del test es verificar que se puedan hacer encuentros luego de haber terminado un encuentro
-        [Test]
-        public void DoTwoEncounterTest()
-        {
-            encounter.AddHero(dwarfTest);
-            encounter.AddEnemy(orcTest);
-            encounter.DoEncounter();
-            encounter.AddHero(knightTest);
-            encounter.AddEnemy(skeletonTest);
-            encounter.DoEncounter();
-            Assert.AreEqual(100, knightTest.Health);
-            Assert.AreEqual(0, skeletonTest.Health);
-        }
-
-        //El objetivo del test es verificar que un character utilizado en un encuentro anterior puede ser parte
-        //de un nuevo encuentro
-        [Test]
-        public void DoTwoEncounterTestWithTheSameCharacter()
-        {
-            encounter.AddHero(dwarfTest);
-            encounter.AddEnemy(orcTest);
-            encounter.DoEncounter();
-            encounter.AddHero(dwarfTest);
-            encounter.AddEnemy(skeletonTest);
-            encounter.DoEncounter();
-            Assert.AreEqual(42, dwarfTest.Health);
-            Assert.AreEqual(0, skeletonTest.Health);
-        }
+        
     }
 }
